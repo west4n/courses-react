31dde463a8b9280a80e2e30f6bb5176c62c2a8cb
Ivan Kurski <iskurskiii@gmail.com>
2021-09-28 22:59:19 +0300

    [ADD] Add classes on components

--- src/components/employers-add-form/employers-add-form.jsx
+++ src/components/employers-add-form/employers-add-form.jsx
@@ -1,20 +1,50 @@
+import {Component} from "react";
 import './employers-add-form.css'
 
-const EmployersAddForm = () => {
+class EmployersAddForm extends Component {
 
-	return (
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			salary: ''
+		}
+	}
 
-		<div className="app-add-form">
-			<h3>Добавьте нового сотрудника</h3>
-			<form action="" className="add-form d-flex">
-				<input type="text" className="form-control new-post-label" placeholder="Как его зовут?" />
-				<input type="number" className="form-control new-post-label" placeholder="З/П в $?" />
-				<button type="button" className="btn btn-outline-light">Добавить</button>
-			</form>
-		</div>
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
 
-	)
 
+
+	render() {
+		const {name, salary} = this.state
+
+		return (
+
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form action="" className="add-form d-flex">
+					<input type="text"
+						   className="form-control new-post-label" placeholder="Как его зовут?"
+						   name="name"
+						   value={name}
+						   onChange={this.onValueChange}
+					/>
+					<input type="number"
+						   className="form-control new-post-label" placeholder="З/П в $?"
+						   name="salary"
+						   value={salary}
+						   onChange={this.onValueChange}
+					/>
+					<button type="button" className="btn btn-outline-light">Добавить</button>
+				</form>
+			</div>
+
+		)
+	}
 }
 
 export default EmployersAddForm;
--- src/components/employers-list-item/employers-list-item.jsx
+++ src/components/employers-list-item/employers-list-item.jsx
@@ -1,27 +1,63 @@
+
+import {Component} from "react";
 import './employers-list-item.css';
 
-const EmployersListItem = ({ name, salary, increase }) => {
+class EmployersListItem extends Component {
 
-	let classNames = "list-group-item d-flex justify-content-between"
-	if (increase) {
-		classNames += " increase"
+	constructor(props) {
+		super(props);
+		this.state = {
+			increase: false,
+			rise: false
+		}
 	}
 
-	return (
-		<li className={classNames}>
-			<span className="list-group-item-label">{name}</span>
-			<input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
-			<div className="d-flex justify-content-center align-items-center">
-				<button type="button" className="btn-cookie btn-sm">
-					<i className="fas fa-cookie"></i>
-				</button>
-				<button type="button" className="btn-trash btn-sm">
-					<i className="fas fa-trash"></i>
-				</button>
-				<i className="fas fa-star"></i>
-			</div>
-		</li>
-	)
+	onIncrease = () => {
+		this.setState(({increase}) => ({
+			increase: !increase
+		}))
+	}
+
+	onRise = () => {
+		this.setState(({rise}) => ({
+			rise: !rise
+		}))
+	}
+
+	render() {
+		const {name, salary} = this.props;
+		const {increase, rise} = this.state;
+
+		let classNames = "list-group-item d-flex justify-content-between"
+		if (increase) {
+			classNames += " increase"
+		}
+		 if (rise) {
+			 classNames += " like"
+		 }
+
+		return (
+			<li className={classNames}>
+				<span
+					className="list-group-item-label"
+					onClick={this.onRise}
+				>{name}</span>
+				<input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+				<div className="d-flex justify-content-center align-items-center">
+					<button type="button"
+							className="btn-cookie btn-sm"
+							onClick={this.onIncrease}
+					>
+						<i className="fas fa-cookie"></i>
+					</button>
+					<button type="button" className="btn-trash btn-sm">
+						<i className="fas fa-trash"></i>
+					</button>
+					<i className="fas fa-star"></i>
+				</div>
+			</li>
+		)
+	}
 
 }
 
